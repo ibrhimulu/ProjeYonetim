8e7614328de2bfc439eb4a9f3ec9d40b2fcc461a
İbrahim Halil Ulugerger <ibrahimhalil9520@gmail.com>
2024-08-19 11:39:26 +0300

    Admin paneli kullanıcı sayfası düzenlendi. ProjectController ayarlandı ve indexleri açıldı.

--- herkesuyurkenkodlama/Controllers/ProjectController.cs
+++ herkesuyurkenkodlama/Controllers/ProjectController.cs
@@ -4,7 +4,11 @@ namespace herkesuyurkenkodlama.Controllers
 {
     public class ProjectController : Controller
     {
-        public IActionResult Index()
+        public IActionResult UserIndex()
+        {
+            return View();
+        }
+        public IActionResult AdminIndex()
         {
             return View();
         }
