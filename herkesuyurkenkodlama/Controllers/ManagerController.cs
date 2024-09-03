c88783f8641a6c373e117854210965bf971f5a19
İbrahim Halil Ulugerger <ibrahimhalil9520@gmail.com>
2024-09-03 16:56:30 +0300

    Manager rolünün gereksinimleri eklendi.

---
+++ herkesuyurkenkodlama/Controllers/ManagerController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace herkesuyurkenkodlama.Controllers
+{
+    [Authorize(Roles = "Admin,Manager")]
+    public class ManagerController : Controller
+    {
+        public IActionResult Projects()
+        {
+            return View();
+        }
+        public IActionResult Tasks()
+        {
+            return View();
+        }
+        public IActionResult Teams()
+        {
+            return View();
+        }
+    }
+}
