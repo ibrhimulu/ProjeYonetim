f5aa3fc050854db9fff3881481f70a7573db9a5d
İbrahim Halil Ulugerger <ibrahimhalil9520@gmail.com>
2024-09-04 13:18:35 +0300

    Manager panelinin proje sayfası tüm müdürlükteki projeleri gösterecek şekilde ayarlandı.

--- herkesuyurkenkodlama/Controllers/ManagerController.cs
+++ herkesuyurkenkodlama/Controllers/ManagerController.cs
@@ -1,15 +1,73 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using AutoMapper;
+using herkesuyurkenkodlama.Contexts;
+using herkesuyurkenkodlama.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace herkesuyurkenkodlama.Controllers
 {
     [Authorize(Roles = "Admin,Manager")]
     public class ManagerController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public ManagerController(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
         public IActionResult Projects()
         {
-            return View();
+            var userIdClaim = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (!int.TryParse(userIdClaim, out var userId))
+            {
+                return BadRequest("Geçersiz kullanıcı ID.");
+            }
+
+            var userDepartmentId = _context.Users
+                .Where(u => u.UserId == userId)
+                .Select(u => u.DepartmentId)
+                .FirstOrDefault();
+
+            var projects = _context.Projects
+                .Where(p => p.DepartmentId == userDepartmentId && p.IsActive == true)
+                .ToList()
+                .Select(x => _mapper.Map<ProjectViewModel>(x))
+                .ToList();
+
+            ViewBag.Users = _context.Users
+                .Select(u => new SelectListItem
+                {
+                    Value = u.UserId.ToString(),
+                    Text = u.Username
+                })
+                .ToList();
+
+            ViewBag.Departments = _context.Mdepartments
+                .Select(d => new SelectListItem
+                {
+                    Value = d.DepartmentId.ToString(),
+                    Text = d.DepartmanName
+                })
+                .ToList();
+
+            ViewBag.SubDepartments = _context.Sdepartments
+                .Select(sd => new SelectListItem
+                {
+                    Value = sd.SubDepartmentId.ToString(),
+                    Text = sd.SubDepartmentName
+                })
+                .ToList();
+
+            return View(projects);
         }
+
         public IActionResult Tasks()
         {
             return View();
